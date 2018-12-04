0b5d259bbc5e6441a6b746c849fb4278ad5f22b9
Espen Hovlandsdal <espen@hovlandsdal.com>
2018-12-04 11:54:21 +0100

    test: add tests for nested quotation marks
    PR-URL: https://github.com/ibmruntimes/yieldable-json/pull/6
    Reviewed-By: Gireesh Punathil <gpunathi@in.ibm.com>
    Reviewed-By: Ravali Yatham <yatraval@in.ibm.com>

--- test/test-parse-basic.js
+++ test/test-parse-basic.js
@@ -3,7 +3,7 @@
 const yj = require('../index');
 const tap = require('tap');
 
-const str = '{"name":"Ila Gould","age":22,"gender":"female"}';
+const str = '{"name":"Ila Gould","age":22,"gender":"female","nested":"\\"value\\""}';
 
 // Make sure the API works well without the optional parameters.
 yj.parseAsync(str, (err, obj) => {
