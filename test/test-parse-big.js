2bc60af5401090fb1001c26e28f8d6612c22dca8
Marc Beaupre <marc.beaupre@autodesk.com>
2019-04-23 16:52:25 +0800

    Test: Use blocked-at to measure how long a single tick takes

--- test/test-parse-big.js
+++ test/test-parse-big.js
@@ -3,6 +3,7 @@
 const yj = require('../index');
 const tap = require('tap');
 
+// 1. Set up really big JSON object
 const RANDOM_STRING = 'abcdefghijklmnopqrstuvwxCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+[]{}\\|;:\'",<.>/?`~';
 const stringLength = Math.pow(2, 10);
 const bigString = new Array(stringLength).fill(RANDOM_STRING).join('');
@@ -18,12 +19,26 @@ const bigObject = new Array(objectSize).fill(undefined).reduce((accumulator) => 
 const bigJSON = JSON.stringify(bigObject);
 console.log('Object size', bigJSON.length);
 
+// 2. Measure how long it takes regular JSON.parse
 const nonYieldParseTime0 = Date.now();
 JSON.parse(bigJSON);
 const nonYieldParseTime1 = Date.now();
 const nonYieldParseElapsedTime = nonYieldParseTime1 - nonYieldParseTime0;
 
-let longestStarvation = 0;
+// 3. Make sure a single tick doesn't block for too long. Uses async-hooks
+const blocked = require('blocked-at');
+let longestTickTime = 0;
+let longestTickStack;
+blocked((blockTime, stack) => {
+  blockTime = parseInt(blockTime, 10); // eslint-disable-line no-param-reassign
+  if (blockTime > longestTickTime) {
+    longestTickTime = blockTime;
+    longestTickStack = stack;
+  }
+});
+
+// 4. Set up a measure of how often the node loop runs and how long between runs (max)
+let longestLoopStarvation = 0;
 let yieldCount = 0;
 let time0 = Date.now();
 const interval = setInterval(() => {
@@ -31,24 +46,31 @@ const interval = setInterval(() => {
   const time1 = Date.now();
   const elapsedTime = time1 - time0;
   time0 = time1;
-  if (elapsedTime > longestStarvation) {
-    longestStarvation = elapsedTime;
+  if (elapsedTime > longestLoopStarvation) {
+    longestLoopStarvation = elapsedTime;
   }
 }, 0);
 
-
-// Make sure the API works well without the optional parameters.
 const yieldParseTime0 = Date.now();
 yj.parseAsync(bigJSON, (error, obj) => {
+  // 5. Measure how long yj.parseAsync takes
   const yieldParseTime1 = Date.now();
   const yieldParseElapsedTime = yieldParseTime1 - yieldParseTime0;
   console.log('Total time', yieldParseElapsedTime);
+
   if (error) {
     tap.fail(error);
   } else {
-    tap.equal(yieldCount > 100, true, `Not enough yielding ${yieldCount}`);
-    tap.equal(longestStarvation < 10, true, `Main thread starved too long ${longestStarvation}`);
-    tap.equal(yieldParseElapsedTime / 10 < nonYieldParseElapsedTime, `Yieldable parse took 10x longer than regular parse ${yieldParseElapsedTime} vs ${nonYieldParseElapsedTime}`);
+    tap.equal(yieldParseElapsedTime / 10 < nonYieldParseElapsedTime, true, `Yieldable parse took 10x longer than regular parse ${yieldParseElapsedTime} vs ${nonYieldParseElapsedTime}`);
+    tap.equal(yieldCount > 10, true, `Not enough yielding ${yieldCount}`);
+    tap.equal(longestLoopStarvation < 100, true, `Loop starved too long ${longestLoopStarvation}`);
+    setTimeout(() => { // need 2 loops for blocked-at to call-back for some reason...
+      setTimeout(() => {
+        tap.equal(longestTickTime < 100, true, `Tick took too long ${longestTickTime}`);
+        // Will print what took that lon
+        // console.log(longestTickStack);
+      });
+    });
   }
   clearInterval(interval);
 });
