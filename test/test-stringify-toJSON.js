e396344fa23706efbf38cf1c183bb2929e04d5e8
Machala, Jan <honza.machala@seznam.cz>
2026-01-16 15:52:16 +0100

    src: Use yieldable variant for toJSON class method

--- test/test-stringify-toJSON.js
+++ test/test-stringify-toJSON.js
@@ -5,11 +5,21 @@ const tap = require('tap');
 
 class TestClass {
   constructor() {
-    this.value = 4;
+    this.bool = true;
+    this.number = 4;
+    this.string = 'string';
+    this.quoted = '"quoted"';
+    this.null = null;
   }
 
   toJSON() {
-    return {value: this.value};
+    return {
+      bool: this.bool,
+      number: this.number,
+      string: this.string,
+      quoted: this.quoted,
+      null: this.null,
+    };
   }
 }
 
@@ -22,6 +32,34 @@ yj.stringifyAsync(test, (err, str) => {
     yj.parseAsync(str, (error, data) => {
       if (!error){
         tap.deepEquals(data, test);
+      } else {
+        tap.fail(error);
+      }
+    });
+  } else {
+    tap.fail(err);
+  }
+});
+
+class TestNestedClass {
+  constructor() {
+    this.value = {data: true, string: 'test', number: 42, array: [1, 2, 3]};
+  }
+
+  toJSON() {
+    return {value: this.value};
+  }
+}
+
+// class with various nested values and custom toJSON method
+const testNested = new TestNestedClass();
+
+yj.stringifyAsync(testNested, (err, str) => {
+  if (!err){
+    tap.equal(JSON.stringify(testNested), str);
+    yj.parseAsync(str, (error, data) => {
+      if (!error){
+        tap.deepEquals(data, testNested);
       } else {
         tap.fail(error);
       }
